a37fdb357ddc26d68880898dc7abc063e5fa837b
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-15 14:58:51 -0400

    i guess i understand this

--- js/challenge.js
+++ js/challenge.js
@@ -4,31 +4,62 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const plusButton = document.getElementById('plus');
   const heartButton = document.getElementById('heart');
   const pauseButton = document.getElementById('pause');
-  const submitButton = document.getElementById('submit');
   const inputBox = document.getElementById('comment-input');
   const commentList = document.getElementById('list');
-  const likeContainer = document.querySelector('.likes')[0]
-  const timerInterval = setInterval(updateTimer, 1000)
+  const likeContainer = document.querySelector('.likes');
+  const submitButton = document.getElementById('submit');
+  const commentForm = document.getElementById("comment-form");
   var counter = 0;
+  const timerInterval = setInterval(updateTimer, 1000);
+  var likeTracker = {};
   var paused = false;
 
   function updateTimer(value=1) {
-    if (paused) {return}
+    if (paused) {return};
 
-    counter += value
+    counter += value;
     clockElement.textContent = counter;
-  }
+  };
 
-  minusButton.addEventListener('click', (event) => {updateTimer(-1)})
-  plusButton.addEventListener('click', (event) => {updateTimer(1)})
+  minusButton.addEventListener('click', (event) => {updateTimer(-1)});
+  plusButton.addEventListener('click', (event) => {updateTimer(1)});
 
   // Handle pause button logic
   pauseButton.addEventListener('click', (event) => {
-    paused = !paused
+    paused = !paused;
     event.target.textContent = (paused) ? "resume" : "pause";
     minusButton.disabled = paused;
     plusButton.disabled = paused;
-  })
+    heartButton.disabled = paused;
+    submitButton.disabled = paused;
+  });
+
+  commentForm.addEventListener('submit', addComment);
+
+  function addComment(event) {
+    event.preventDefault();
+    const comment = document.createElement("p");
+    comment.textContent = inputBox.value;
+    commentList.append(comment);
+  }
+
+  heartButton.addEventListener('click', (event) => {
+    if (!(counter in likeTracker)) {
+      likeTracker[counter] = {value:0};
+    };
+    likeTracker[counter].value += 1;
+    addLike();
+  });
+
+  function addLike() {
+    let likeInstance = document.getElementById(`like-number-${counter}`);
+    if (!likeInstance) {
+      likeInstance = document.createElement("li");
+      likeInstance.id = `like-number-${counter}`;
+      likeContainer.append(likeInstance);
+    }
+    likeInstance.textContent = `${counter} has been liked ${likeTracker[counter].value} times`;
+  }
 
 
 }) // End of Document Loaded
