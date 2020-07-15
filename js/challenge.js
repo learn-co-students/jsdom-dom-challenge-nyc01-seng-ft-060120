dc847264e01676d0d82afebaf188ae1e53dccdd4
berellevy <berellevy@gmail.com>
2020-07-15 14:26:29 -0400

    added plus and minus

--- js/challenge.js
+++ js/challenge.js
@@ -1,12 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(e) {
     const counterNumber = document.querySelector('#counter')
-    console.log(counterNumber.innerText)
-    function increment(){
+    const body = document.querySelector('body')
+
+    body.addEventListener('click', function(e) {
+        if (e.target.id === 'minus') { decrement() }
+        if (e.target.id === 'plus') { increment() }
+        if (e.target.id === 'heart') { decrement() }
+        if (e.target.id === 'minus') { decrement() }
+
+    })
+
+    function increment() {
         counterNumber.innerText = parseInt(counterNumber.innerText, 10) + 1;
     }
 
-    
-    setInterval( increment , 1000)
+
+    function decrement() {
+        counterNumber.innerText = parseInt(counterNumber.innerText, 10) - 1;
+    }
+
+
+
+
+
+
+
+
+    setInterval(increment, 1000)
 
 
 
