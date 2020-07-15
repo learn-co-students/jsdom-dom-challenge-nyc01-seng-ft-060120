4352669319dad93547550c292a947f24ac449db3
SudoRudo <r.guerreronyc@gmail.com>
2020-07-15 15:44:00 -0400

    Id comparisons work

--- js/challenge.js
+++ js/challenge.js
@@ -1,23 +1,45 @@
 document.addEventListener("DOMContentLoaded", function(e) {
     const counterNumber = document.querySelector('#counter')
     const body = document.querySelector('body')
+    const likesUl = document.querySelector('.likes')
+    console.dir(likesUl.childNodes)
+
+    function increment() {
+        counterNumber.innerText = parseInt(counterNumber.innerText, 10) + 1;
+    };
+    function decrement() {
+        counterNumber.innerText = parseInt(counterNumber.innerText, 10) - 1;
+    };
+
+    
+    function likeNumber() {
+        likesArray = Array.from(likesUl.childNodes)
+        idsArray = likesArray.map(li => li.id)
+        potentialChildId = counterNumber.innerText
+
+        if (idsArray.includes(potentialChildId)){
+            const likedNumberSpan = likedNumberLi.querySelector('span');
+            likedNumberSpan.innerText = parseInt(likedNumberSpan.innerText) + 1;
+        }
+
+        else {    
+            const newLikeLi = document.createElement('li');
+            newLikeLi.id = counterNumber.innerText;
+            newLikeLi.innerHTML = `${counterNumber.innerText} was liked <span>1</span> time`;
+            likesUl.appendChild(newLikeLi);
+        };
+    
+    }
+
 
     body.addEventListener('click', function(e) {
         if (e.target.id === 'minus') { decrement() }
         if (e.target.id === 'plus') { increment() }
-        if (e.target.id === 'heart') { decrement() }
+        if (e.target.id === 'heart') { likeNumber() }
         if (e.target.id === 'minus') { decrement() }
 
     })
 
-    function increment() {
-        counterNumber.innerText = parseInt(counterNumber.innerText, 10) + 1;
-    }
-
-
-    function decrement() {
-        counterNumber.innerText = parseInt(counterNumber.innerText, 10) - 1;
-    }
 
 
 
